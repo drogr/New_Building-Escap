b43cac26e2bd8d0b7f64cd18f33fb72a9a2f20db
jeff morris <jeff morris>
2016-05-09 09:54:35 -0600

    Refactored

--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -43,4 +43,10 @@ private:
 
 	// Return hit for for first physics body in reach
 	const FHitResult GetFirstPhysicsBodyInReach();
+
+	//returns current start of reach line
+	FVector GetReachLineStart();
+
+	//returns current end of reach line
+	FVector GetReachLineEnd();
 };
